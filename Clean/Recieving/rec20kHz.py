f376e5d588caa61c0b012436cef6b8f5e763b33f
Tyler Martin <trm32@cam.ac.uk>
2025-05-14 22:35:03 +0100

    continuation sync code

--- Clean/Recieving/rec20kHz.py
+++ Clean/Recieving/rec20kHz.py
@@ -18,35 +18,39 @@ def read_gpio(gpio, level, tick):
     timearray.append(tick)
     dataarray.append(level)
 
-# Set up the callback to read the GPIO pin at high speed
+# Call the callback to read the GPIO pin at high speed
 cb = pi.callback(GPIO_PIN, pigpio.EITHER_EDGE, read_gpio)
 
 # Run for a specified duration
 duration = 0.5  # seconds
-time.sleep(duration)
-
+bitlength = 50    #every 0.1 seconds resync, 0.1/50μs = 2000 bits (might even do more resyncs),
+time.sleep(duration) #duration in seconds   #20khz is 50μs, μs seems to be unit of time array, T = 0.00005s
 # Stop the callback and pigpio connection
 cb.cancel()
 pi.stop()
 
-for i in range(len(timearray)-1):       #20khz is 50μs, μs seems to be unit of time array, T = 0.00005s
-    if i==0:                            #every 0.1 seconds resync, 0.1/50μs = 2000 bits (might even do more resyncs), Lets do 1000T = 0.05s or 50*1000=50,000μs
+for i in range(len(timearray)-1):       
+    if i==0:                            
         timestart=timearray[0]          #work out time difference, sum it to remainder then allocate bits (same as sampling every....)
         remainder=0                     #So the thinking is time is continuous line, you add onto remainder to as see how many new bits the time spans,   
         resyncCounter = 0               #take those reading points as the current value, leave the left over
+        resynctrigger =0                #test value for how large remainder
 
     #calculate time length (length of that mode )    
-    itime = timearray[i+1] - timearray[i]
-    totaltime = itime + remainder
-    resyncCounter += itime 
-    for 
+    itime = timearray[i+1] - timearray[i]  #length of this mode (0,1)
+    totaltime = itime + remainder          #time passed since last reading
+    
+    bitCoverage = totaltime // bitlength
         
 
-    #if (dataarray[i] == 1):    
-        
-    if(resyncCounter > 50000):#check resync
-        if (remainder> 15):
+    #if (dataarray[i] == 1): 
+    #check resync   
+    resyncCounter += itime        #add current length      
+    if(resyncCounter > bitlength*1000):    #after 1000 bits resync the to tick change time (might make more frequent)  
+        if (remainder> 14):                #Lets do 1000T = 0.05s or 50*1000=50,000μs
             #add bit perhaps?
+            resynctrigger = 
+
 
 
 
